e93618f6f22737e34604d557777a34105d436910
Rodrigo <rcbm539@gmail.com>
2017-07-20 00:16:21 -0300

    List clients in table

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -12,14 +12,24 @@ import { Client } from '../../models/Client'
 export class ClientsComponent implements OnInit {
 
   clients: Client[]
+  totalOwed: number
 
   constructor(public clientService: ClientService) { }
 
   ngOnInit() {
     this.clientService.getClients().subscribe(clients => {
       this.clients = clients
-      console.log(this.clients)
+      this.getTotalOwed()
     })
+  }
+
+  getTotalOwed(){
+    let total = 0
+    for(let i = 0; i < this.clients.length; i++){
+      total += this.clients[i].balance //soma todos os itens da balanca
+    }
+    this.totalOwed = total
+    console.log(this.totalOwed)
   }
 
 }
