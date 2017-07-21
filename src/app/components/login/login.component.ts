43b71d583c9277143eb10c8c9e567bd28c17ebed
Rodrigo <rcbm539@gmail.com>
2017-07-20 22:01:49 -0300

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
